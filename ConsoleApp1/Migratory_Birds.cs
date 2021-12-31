66b17f96e302b75e3a03459825d6168a87d18b41
imtiajahammad <imtiajahammad.official@gmail.com>
2022-01-01 01:00:48 +0600

    Migratory_Birds problem solution

--- ConsoleApp1/Migratory_Birds.cs
+++ ConsoleApp1/Migratory_Birds.cs
@@ -15,12 +15,43 @@ namespace ProblemSolvings
         ///    
         public Migratory_Birds()
         {
-            int res=migratoryBirds(new List<int>(){1,1,2,2,3});
+            //int res=migratoryBirds(new List<int>(){1,1,2,2,3});//result is one
+            //int res=migratoryBirds(new List<int>(){1 ,4 ,4 ,4 ,5 ,3});//result is 4
+            int res=migratoryBirds(new List<int>(){1 ,2 ,3 ,4 ,5 ,4 ,3 ,2 ,1 ,3, 4});//result is 3
+
+
             Console.WriteLine("Result is {0}",res);
         }
         public static int migratoryBirds(List<int> arr)
         {
-            return 0;
+            int[] array=arr.ToArray();
+            int selectedkey=0;
+            int selectedValue=0; 
+            Dictionary<int,int> dics=new Dictionary<int, int>();
+            for(int i=0;i<array.Length;i++)
+            {
+                Console.WriteLine(array[i]);
+                if(dics.ContainsKey(array[i]))
+                {
+                    var val=dics[array[i]];
+                    dics[array[i]]=val+1;
+                }
+                else
+                {
+                    dics.Add(array[i],1);
+                }
+            }
+            foreach(var a in dics){
+                if(selectedValue<a.Value){
+                    selectedValue=a.Value;
+                    selectedkey=a.Key;
+                }
+                else if(selectedValue==a.Value && selectedkey >a.Key){
+                    selectedkey=a.Key;
+                    selectedValue=a.Value;
+                }
+            }
+            return selectedkey;
         }
 
 
